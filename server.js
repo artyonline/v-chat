b8d2a47b1f39a24b49dcf6b3b43be1ca6822d3cd
artyonline <minuka.2015217@iit.ac.lk>
2020-08-06 12:36:19 +0530

    server routes

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4} = require('uuid')
+
+
+app.set('view engine', 'ejs');  
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World");
-})
+    res.status(200).render('home');
+});
+
+app.get('/createRoom', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.status(200).render('room', { roomId: req.room});
+});
 
 server.listen(3030);
